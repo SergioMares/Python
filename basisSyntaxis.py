d46e36ae7b4ab348c8ffa58f02d7f1b217bb7109
Sergio Mares Martínez <63088893+SergioMares@users.noreply.github.com>
2020-06-23 15:43:40 -0500

    basics of python language
    somo basic syntax of python.

---
+++ basisSyntaxis.py
@@ -0,0 +1,15 @@
+#You don't need to use a ';' at the end of every sentence
+#Also You don't need to put a variable type in python, it does automatically. like...
+numberOne = 1
+numberTwo = 1.123465789
+#Exponentiation ... 3^2
+3 ** 2
+
+#strings
+string = 'helloWorld'
+stringWithTab = "here's a \t tab";
+stringWithLineBreak = "here's a \n line break"
+
+print(string)
+print(stringWithTab)
+print(stringWithLineBreak)
