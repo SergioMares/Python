1451e1e55d7e68b1b4cf37974e12ff9a89894aad
Sergio Mares Martínez <63088893+SergioMares@users.noreply.github.com>
2020-06-29 16:28:38 -0500

    Update controlStructures.py
    added a for loop and some methods

--- controlStructures.py
+++ controlStructures.py
@@ -19,9 +19,32 @@ else:
 c = 0
 while c <= 5:
     c+=1
-#    if (c==4):
+    if (c==4):
 #       print("Break te loop when c is 4",)
 #       break
+        print("here comes the continue")
+        continue
+        print("this message will never out because of the continue")
     print("c value is:",c)
 else:
-    print("the loop has ended and c is", c)
+    print("Se ha completado toda la iteración y c vale", c)
+    
+############################
+#for loop. it´s a foreach loop or a auto-indexed loop. to change a value in a data structure, you've to access by the index. the for loop also works with strings
+list = ["asf",'qwer',"zxcv"]
+for string in list:
+    print(string)
+
+#you can get the index adding a parameter to the for loop 
+numbers = [2, 234, 32, 423, 25, 566, 97, 812, 91, 310]
+for index,number in enumerate(numbers):
+    print("number:" ,number)#the value in the list
+    print("index:",index)#pretty explains itself
+numeros
+    
+#the range method will return a list of numbers. 
+#range(x,y,z) where 'x' is the beggining (if you put nothing strats from 0) 'y' is the end and 'z' the steps (if nothing will be 1 by 1)    
+for x in range(2, 30, 3):
+  print(x)   
+    
+    
